18640d480143546696435fe1aa705d91653f2a07
kaputin <kaputinevgeny@gmail.com>
2018-09-05 23:06:20 +0300

    Новая форма

--- js/form.js
+++ js/form.js
@@ -3,14 +3,6 @@
   var arrival = popup.querySelector("[name=arrival-date]");
   var Departure = popup.querySelector("[name=Departure-date]");
 
-  var isStorageSupport = true;
-  var storage = "";
-
-  try {
-    storage = localStorage.getItem("arrival");
-  } catch (err) {
-    isStorageSupport = false;
-  }
 
   link.addEventListener("click", function (evt) {
     evt.preventDefault();
@@ -21,6 +13,8 @@
   popup.addEventListener("submit", function (evt) {
     if (!arrival.value || !Departure.value) {
       evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
       popup.classList.add("modal-error");
     } else {
       if (isStorageSupport) {
